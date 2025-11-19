8b2d4e25fd9310a3fbda143a8d785ed1a0c47723
SosoMaster <sashgamergatev@gmail.com>
2025-11-19 10:41:18 +0200

    asd

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Tile_Repair.Controller;
+using Tile_Repair.Models;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        private TileRepairService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new TileRepairService();
+        }
+
+        [Test]
+        public void TileRepair_Test_TestingThatCalculateReturnsCorrectValues()
+        {
+
+            var input = new TileRepairInput(20, 5, 4, 1, 2);
+            var result = _service.Calculate(input);
+
+            Assert.That(result.TilesNeeded, Is.EqualTo(19.9));
+            Assert.That(result.TimeNeeded, Is.EqualTo(3.98));
+        }
+    }
+}
